ecf1258f1cddf38e74666f0dfe75e7fdd3fbb3de
vanek123 <vanjaaleskevic@mail.ru>
2022-02-17 12:48:48 +0200

    js try

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,55 +1,42 @@
-const POP_Up = document.getElementById('popUp');
-let ieraksti = [];
+const POP_UP = document.getElementById('fields');
+let gramatas = [];
 
 window.addEventListener('load', () => {
-    ieraksti = JSON.parse(localStorage.getItem("ieraksti") || "[]");
-    console.log(ieraksti)
+    gramatas = JSON.parse(localStorage.getItem("gramatas") || "[]");
+    console.log(gramatas)
     render();
 });
 
-const ieraksts = document.querySelector('#saraksts')
-
-ieraksts.addEventListener('click', (e) => {
-    if(e.target.className == 'delete'){
-      const li = e.target.parentElement;
-      li.parentNode.removeChild(li);
-      ieraksti.splice(li, 1);
-      localStorage.setItem('ieraksti',JSON.stringify(ieraksti));
-    }
-});
-
-document.getElementById('jaunsPirkums').addEventListener('click', () => {
-    POP_Up.style.display = 'block';
-});
-
 document.getElementById('pievienot').addEventListener('click', () => {
-    POP_Up.style.display = 'none';
+    POP_UP.style.display = 'block';
+})
 
-    let pirkums = {nosaukums2: nosaukums2.value, adrese2: adrese2.value};
+document.getElementById('pievienotGramatu').addEventListener('click', () => {
+    POP_UP.style.display = 'none';
+
+    let gramata = {nosaukums2: nosaukums2.value, adrese2: adrese2.value};
 
     nosaukums2.value = "";
     adrese2.value = "";
 
-    ieraksti.push(pirkums);
+    gramatas.push(gramata);
 
     render();
- });
+})
 
- function render() {
-    let saraksts = document.getElementById('saraksts');
-    saraksts.innerHTML = "";
+function render() {
+    let biblioteka = document.getElementById('saraksts');
+    biblioteka.innerHTML = "";
 
-    for(let i = 0; i < ieraksti.length; i++) {
-        let pirkums = `
-        <div class="pirkums">
-            <p>Prece: ${ieraksti[i].nosaukums2}</p>
-            <p>Daudzums: ${ieraksti[i].adrese2}</p>
-            <button class="delete">Dzēst</button>
-        </div> `
+    for(let i = 0; i < gramatas.length; i++) {
+        let gramata = `
+        <div class="gramata">
+            <h3>Virsraksts: ${gramatas[i].nosaukums2}</h3>
+            <h4>Autors: ${gramatas[i].adrese2}</h4>
+        </div>`;
 
-        saraksts.innerHTML += pirkums;
-    };
+        biblioteka.innerHTML += gramata;
+    }
 
-    localStorage.setItem("ieraksti", JSON.stringify(ieraksti))
-
-};
+    localStorage.setItem("gramatas", JSON.stringify(gramatas))
+}
